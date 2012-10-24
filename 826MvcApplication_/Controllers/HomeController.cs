0b6ae86bd9234c7fa8b69a5211e3520cb3c8f503
hello kitty <v-hozeng@hw-hozeng-vm05.fareast.corp.microsoft.com>
2012-10-24 15:19:48 +0800

    change1 commit

--- 826MvcApplication_/Controllers/HomeController.cs
+++ 826MvcApplication_/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace _826MvcApplication_.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to ASP.NET MVC!haha";
 
             return View();
         }
